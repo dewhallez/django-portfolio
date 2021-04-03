98860b4c543479eacace5ed045b23e94bc043255
dewhallez <dewhalles@gmail.com>
2021-04-03 02:10:26 -0400

    update projects static folder

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -31,15 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    
+    'projects',
+    'blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projects',
-    'blog',
+    
     
 ]
 
